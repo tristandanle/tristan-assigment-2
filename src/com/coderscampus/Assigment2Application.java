b5c616ef4f5793d7cb4d107eb1c5e3e7efafd01f
Tristan <letristandan@gmail.com>
2022-03-02 13:17:54 -0500

    Assigment2Application changed

--- src/com/coderscampus/Assigment2Application.java
+++ src/com/coderscampus/Assigment2Application.java
@@ -4,6 +4,12 @@ public class Assigment2Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// generate a random number
+		
+		// and collect user input
+		
+		//etc. 
 
 	}
 
