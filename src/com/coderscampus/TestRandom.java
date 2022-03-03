d2cf14ad68f904402947fc007a7e65cd10883aa1
Tristan Dan Le <letristandan@gmail.com>
2022-03-03 01:00:24 -0500

    Update TestRandom.java

--- src/com/coderscampus/TestRandom.java
+++ src/com/coderscampus/TestRandom.java
@@ -1,19 +1,3 @@
-package com.coderscampus;
 
-import java.util.Random;
-
-public class TestRandom {
-
-	public static void main(String[] args) {
-	    
-	        // what is our range?
-	        int max = 100;
-	        int min = 1;
-	      // create instance of Random class
-	        Random randomNum = new Random();
-	        int showMe = min + randomNum.nextInt(max);
-	        System.out.println(showMe);
-	      }
-	
 
 }
