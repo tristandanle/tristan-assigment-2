92e1bdbf02558110ce83b792a83510fa3f27630c
Tristan Dan Le <letristandan@gmail.com>
2022-03-03 01:00:48 -0500

    Update Assigment2Application.java

--- src/com/coderscampus/Assigment2Application.java
+++ src/com/coderscampus/Assigment2Application.java
@@ -3,13 +3,7 @@ package com.coderscampus;
 public class Assigment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		// generate a random number
-		
-		// and collect user input
-		
-		//to be continued
 
 	}
 
